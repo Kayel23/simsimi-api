e2a4b5575e25b535152d03d12eb69dc368141884
French Mangigo <frenchmangigo@gmail.com>
2024-07-28 09:44:42 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+const responses = {
+    "hello": "Hi there!",
+    "how are you": "I'm just a bot, but I'm doing fine!",
+    "bye": "Goodbye!",
+    "miss ko na siya": "wala akong pake",
+    "sama mo naman": "masama talaga ako"
+};
+
+app.get('/sim', (req, res) => {
+    const userMessage = req.query.q.toLowerCase();
+    const botResponse = responses[userMessage] || "Sorry, I don't understand that.";
+    
+    res.json({ respond: botResponse });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
